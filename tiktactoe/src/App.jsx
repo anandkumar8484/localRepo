a216ebfbc033ce282b788862eb4f8fc0d2378326
anandkumar4848 <vishwakarmavanandkumar@gmail.com>
2025-09-22 20:33:33 +0530

    push project files

---
+++ tiktactoe/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TikTacToe from './components/TikTacToe/TikTacToe'
+
+
+const App = () => {
+  return (
+    <div>
+      <TikTacToe/>
+    </div>
+  )
+}
+
+export default App
